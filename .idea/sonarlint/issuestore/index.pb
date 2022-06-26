b91fee53ac480be5819cef0dd0c8e28918bb53cd
maxdexter <maxdexter@bk.ru>
2022-06-26 11:41:29 +0300

    Небольшие правки по валидации полей

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 t
 Dapp/src/main/java/ru/maxdexter/albumsearch/presenter/MainActivity.kt,e\6\e6dbd9c82e36d6cd89603f961d6dbb85687f0f53
--- app/src/main/java/ru/maxdexter/albumsearch/presenter/fragments/registration/RegistrationFragment.kt
+++ app/src/main/java/ru/maxdexter/albumsearch/presenter/fragments/registration/RegistrationFragment.kt
@@ -1,25 +1,57 @@
 package ru.maxdexter.albumsearch.presenter.fragments.registration
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import ru.maxdexter.albumsearch.R
+import ru.maxdexter.albumsearch.databinding.RegisterFragmentBinding
+import ru.maxdexter.albumsearch.domain.common.ValidationHelper
+import ru.maxdexter.albumsearch.presenter.baseclass.BaseFragment
 
-class RegistrationFragment : Fragment() {
+class RegistrationFragment :
+    BaseFragment<RegisterFragmentBinding>(RegisterFragmentBinding::inflate) {
 
-    companion object {
-        fun newInstance() = RegistrationFragment()
-    }
 
     private lateinit var viewModel: RegistrationViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.register_fragment, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.etBirthday.apply {
+            isEnabled = false
+            this.setOnClickListener {
+                Toast.makeText(requireContext(), "sdjkbc", Toast.LENGTH_SHORT).show()
+                val listener = DatePickerDialog.OnDateSetListener { datePicker, i, i2, i3 ->
+
+                }
+                DatePickerDialog(requireContext(),listener,2022,6,26).show()
+            }
+        }
+
+
+        binding.etEmail.doOnTextChanged { text, start, before, count ->
+            when (ValidationHelper.isValid(text.toString(), ValidationHelper.EMAIL_PATTERN)) {
+                true -> {}
+                else -> {
+                    getString(R.string.et_email_error).also { binding.etEmail.error = it }
+                }
+            }
+
+        }
+
+        binding.etPassword.doOnTextChanged { text, start, before, count ->
+            when (ValidationHelper.isValid(text.toString(), ValidationHelper.PASSWORD_PATTERN)) {
+                true -> {}
+                else -> {
+                    getString(R.string.error_password_validation).also {
+                        binding.etPassword.error = it
+                    }
+                }
+            }
+        }
     }
+
 
 }
